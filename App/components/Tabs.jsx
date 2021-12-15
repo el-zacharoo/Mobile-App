97c1160fd141125bd037014f0f7e4c63ec95633c
Your Name <you@example.com>
2021-12-16 10:12:58 +1300

    tab types

--- App/components/Tabs.jsx
+++ App/components/Tabs.jsx
@@ -6,19 +6,52 @@ import { Button } from 'react-native-paper';
 import { theme } from '../theme/index';
 
 export const Tabs = (props) => {
-    const { children, color } = props;
-    return (
-        <TouchableOpacity>
-            <Button {...props} style={{
-                borderTopColor: color ? color : theme.colors.background,
-                borderTopWidth: 3,
-                paddingTop: 4,
-                borderRadius: 0,
-                marginLeft: 1,
-                marginRight: 1
-            }} mode="text" color={color ? color : theme.colors.primary}>
-                {children}
-            </Button>
-        </TouchableOpacity>
-    )
+    const { children, color, orientation } = props;
+    switch (orientation) {
+        case 'bottom':
+            return (
+                <TouchableOpacity>
+                    <Button {...props} style={{
+                        borderTopColor: color ? color : theme.colors.background,
+                        borderTopWidth: 3,
+                        paddingTop: 4,
+                        borderRadius: 0,
+                        marginLeft: 1,
+                        marginRight: 1
+                    }} mode="text" color={color ? color : theme.colors.primary}>
+                        {children}
+                    </Button>
+                </TouchableOpacity>
+            )
+        case 'top':
+            return (
+                <TouchableOpacity>
+                    <Button {...props} style={{
+                        borderBottomColor: color ? color : theme.colors.background,
+                        borderBottomWidth: 3,
+                        paddingBottom: 4,
+                        borderRadius: 0,
+                        marginLeft: 1,
+                        marginRight: 1
+                    }} mode="text" color={color ? color : theme.colors.primary}>
+                        {children}
+                    </Button>
+                </TouchableOpacity>
+            )
+        default:
+            return (
+                <TouchableOpacity>
+                    <Button {...props} style={{
+                        borderTopColor: color ? color : theme.colors.background,
+                        borderTopWidth: 3,
+                        paddingTop: 4,
+                        borderRadius: 0,
+                        marginLeft: 1,
+                        marginRight: 1
+                    }} mode="text" color={color ? color : theme.colors.primary}>
+                        {children}
+                    </Button>
+                </TouchableOpacity>
+            )
+    }
 }
