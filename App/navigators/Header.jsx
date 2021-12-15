1e4787100d19582a26eb61ed8662ad5fc9f72dd8
Your Name <you@example.com>
2021-12-16 11:51:23 +1300

    cutom heading

--- App/navigators/Header.jsx
+++ App/navigators/Header.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 
 import { Appbar } from 'react-native-paper';
+import { StyleSheet } from 'react-native';
+
+import { theme } from '../theme/index';
 
 
 export const Header = () => {
     return (
-        <Appbar.Header>
-            <Appbar.Content title="My awesome app" />
+        <Appbar.Header style={styles.heading} >
+            <Appbar.Content title="Mobile-App" />
         </Appbar.Header>
     )
 }
+
+const styles = StyleSheet.create({
+    heading: {
+        backgroundColor: theme.colors.primary,
+    },
+});
